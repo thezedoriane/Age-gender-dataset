29606a9728839cf4433e59d1b11f201ed8bc3684
thezedoriane <72502637+thezedoriane@users.noreply.github.com>
2021-04-15 09:38:32 +0200

    Update Collecte_des_données.py

--- Collecte_des_données.py
+++ Collecte_des_données.py
@@ -12,10 +12,13 @@ htmldata=getdata(str(input()))
 soup = BeautifulSoup(htmldata, 'html.parser') 
 
 #récupération de la date de naissance: année
-ch9=soup.get_text()           #récupération du texte
-deb=ch9.index("Naissance en") #recherche l'emplacement de "Naissance en"
+#récupération du texte
+ch9=soup.get_text()        
+#recherche l'emplacement de "Naissance en"
+deb=ch9.index("Naissance en") 
 naissance=ch9[deb:deb+40]
-naissance.strip(' ')          #retire les espaces au début et à la fin du texte sélectionné
+#retire les espaces au début et à la fin du texte sélectionné
+naissance.strip(' ')          
 
 def fin_date(date):
     '''Renvoie le nombre de caractère avant les guillemets.'''
@@ -26,8 +29,10 @@ def fin_date(date):
     
 dnait=naissance[13:fin_date(naissance)]
 date_naissance=dnait.strip(' ')
-date_naissance=date_naissance.split(' ') #créer une liste contenant chaque mot
-date_naissance=date_naissance[1]         #on récupère l'année de naissance
+#créer une liste contenant chaque mot
+date_naissance=date_naissance.split(' ') 
+#on récupère l'année de naissance
+date_naissance=date_naissance[1]        
 print(date_naissance)
 
 #image de la personne
@@ -39,8 +44,34 @@ for item in soup.find_all('img'):
         print(image_interet)
         
 #date photo: année
-fin=ch9[5000:10000].index("Nom de naissance")  #on recherche l'indice de "Nom de naissance"
-date_photo=ch9[5000+fin-21:5000+fin-17]        #récupération de l'année où a été prise la photo
+#on recherche l'indice de "Nom de naissance"
+fin=ch9[5000:10000].index("Nom de naissance") 
+#récupération de l'année où a été prise la photo
+date_photo=ch9[5000+fin-21:5000+fin-17]        
 print(date_photo)
 
-age=int(date_photo)-int(date_naissance) #calcul de l'âge de la personne sur la photo
+#calcul de l'âge de la personne sur la photo
+age=int(date_photo)-int(date_naissance) 
+
+#demande du genre et de l'ethnie de la personne à l'utilisateur et ransformation en str
+print('Quel est le genre ? Noter: H ou F')
+genre=str(input())
+print("Quel est l'ethnie ? Entrer Blanc, Noir, Indien, Asie de l'Est, Asie du Sud-Est, Moyen-Orient ou Latino")
+ethnie=str(input())
+
+#création de l'entête du fichier csv
+entetes=[u'photo', u'age', u'gender', u'ethnie'] 
+valeurs=[]
+#on crée une ligne de valeur contenant l'url de l'image, l'age de la personne son genre et son etnhie, chaque donnée est un str
+valeurs.append([image_interet, str(age), genre, ethnie])
+
+#on ouvre le fichier dataset qui se crée s'il n'existe pas, pour écrire à la suite des données déjà insérées
+dataset=open('dataset.csv','a')             
+
+for valeur in valeurs:
+    #on insère la ligne écrite dans valeurs dans le fichier csv
+     ligne = ";".join(valeur) + "\n"
+     dataset.write(ligne)
+
+#fermeture du fichier csv
+dataset.close()
